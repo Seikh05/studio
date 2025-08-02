068ed9557c96020d2deef93b229dcfeb97146dfc
Seikh Mustakim <seikhsouvagyamustakim@gmail.com>
2025-08-02 15:42:57 +0000

    bro i was deploying this app in vercel, but while logging in i cant sign

---
+++ src/next.config.ts
@@ -0,0 +1,36 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1754111148651.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev',
+    ],
+  },
+};
+
+export default nextConfig;
