37bf5e8a2cf4842c85950a2f68ea72d435e4f26c
Seikh Mustakim <seikhsouvagyamustakim@gmail.com>
2025-08-02 16:30:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +10,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/diqgquom2/image/upload/',
     remotePatterns: [
       {
         protocol: 'https',
