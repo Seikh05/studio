fc884ba909ffe74695adcb1a99e0284219b87b1f
sujalcharati <sujalc024@gmail.com>
2025-08-16 23:55:01 +0530

    fetch update in fe & cors issue in be

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,23 +9,34 @@ import (
 	"github.com/Seikh05/studio/internal/handlers"
 	"github.com/Seikh05/studio/internal/models"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
 
 	database.ConnectDB()
 
-	database.DB.AutoMigrate(&models.User{})
+	err := database.DB.AutoMigrate(&models.User{})
 
+	if err != nil {
+		log.Fatalf("Error running automigrate for model: %v", err)
+	}
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
 	r.HandleFunc("/health", handlers.HealthHandler).Methods("GET")
-	r.HandleFunc("/register",handlers.Register).Methods("POST")
-	r.HandleFunc("/login",handlers.Login).Methods("GET")
+	r.HandleFunc("/register", handlers.Register).Methods("POST")
+	r.HandleFunc("/login", handlers.Login).Methods("POST")
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:9002"}, 
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(r)
 
 	fmt.Println("Server is starting on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
-
-
--- server/go.mod
+++ server/go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/rs/cors v1.11.1 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
